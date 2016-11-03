d65983a5899caf424eed0fbd30c74f761d952ecf
Cezar Sas <cezar.sas@gmail.com>
2016-11-03 22:36:34 +0100

    Removed java folder

--- src/main/java/core/Loader.java
+++
@@ -1,7 +0,0 @@
-package core;
-
-/**
- * Created by Darius on 19/10/2016.
- */
-public class Loader {
-}
--- src/main/java/core/WebPage.java
+++
@@ -1,7 +0,0 @@
-package core;
-
-/**
- * Created by Darius on 20/10/2016.
- */
-public class WebPage {
-}
